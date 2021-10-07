00f0d9ae8385fc8c7e882c31bbf287619d89c7f3
ymikiii <1271311538@qq.com>
2021-10-07 12:46:57 +0800

    init

---
+++ src/test.js
@@ -0,0 +1,332 @@
+const string = `
+.skin *{box-sizing: border-box;margin: 0;padding: 0;}
+.skin *::before, .skin *::after{box-sizing: border-box;}
+
+.skin {
+background: rgba(23, 155, 231, 0.88);
+min-height: 50vh;
+position: relative;
+}
+
+.face_01 {
+margin: 50px auto;
+position: relative;
+width: 420px;
+height: 400px;
+background-color: #1E90FF;
+border: 2px solid #000;
+border-radius: 57% 57% 55% 55%;
+}
+
+.face_02 {
+position: absolute;
+bottom: 0;
+left: 30px;
+width: 360px;
+height: 300px;
+background-color: #FFF;
+border: 2px solid #000;
+border-radius: 60% 60% 55% 55%;
+}
+
+.eye_box {
+position: absolute;
+top: -45px;
+right: 90px;
+width: 180px;
+height: auto;
+}
+
+.big_eye_left,
+.big_eye_right {
+width: 90px;
+height: 110px;
+background-color: #FFF;
+border: 2px solid #000;
+border-radius: 56%;
+}
+
+.big_eye_left {
+float: left;
+}
+
+.big_eye_right {
+float: right;
+}
+
+.small_eye_left,
+.small_eye_right {
+position: absolute;
+top: 50px;
+width: 20px;
+height: 30px;
+background-color: #000;
+border-radius: 70%;
+}
+
+.small_eye_left {
+left: 60px;
+}
+
+.small_eye_right {
+right: 60px;
+}
+
+.nose_01 {
+position: absolute;
+top: 47px;
+left: 151px;
+width: 50px;
+height: 50px;
+background-color: #FF0000;
+border: 2px solid #000;
+border-radius: 50%;
+}
+
+.nose_01_shadow {
+position: absolute;
+top: 10px;
+left: 25px;
+width: 15px;
+height: 15px;
+background-color: #FFF;
+border-radius: 50%;
+box-shadow: 0px 0px 2px 2px rgba(255, 255, 255, .4);
+}
+
+.nose_02 {
+height: 130px;
+width: 2px;
+position: absolute;
+top: 95px;
+left: 176px;
+border-left: 2px solid #000;
+}
+
+.mouth {
+position: absolute;
+top: 25px;
+left: 60px;
+height: 200px;
+width: 240px;
+border-bottom: 2px solid #000;
+border-radius: 0 0 45% 45%;
+}
+
+.whisker_left,
+.whisker_right {
+position: absolute;
+top: 60px;
+width: 180px;
+height: 180px;
+}
+
+.whisker_left {
+left: 0;
+}
+
+.whisker_right {
+right: 0;
+}
+
+.whisker_left span:nth-child(1),
+.whisker_right span:nth-child(1) {
+position: absolute;
+top: 30px;
+height: 60px;
+width: 100px;
+border-top: 2px solid #000;
+}
+
+.whisker_left span:nth-child(1) {
+left: 30px;
+-webkit-transform: rotate(20deg);
+transform: rotate(20deg);
+}
+
+.whisker_right span:nth-child(1) {
+right: 30px;
+-webkit-transform: rotate(-20deg);
+transform: rotate(-20deg);
+}
+
+.whisker_left span:nth-child(2),
+.whisker_right span:nth-child(2) {
+position: absolute;
+top: 60px;
+height: 60px;
+width: 100px;
+border-top: 2px solid #000;
+}
+
+.whisker_left span:nth-child(2) {
+left: 35px;
+}
+
+.whisker_right span:nth-child(2) {
+right: 35px;
+}
+
+.whisker_left span:nth-child(3),
+.whisker_right span:nth-child(3) {
+position: absolute;
+top: 90px;
+height: 60px;
+width: 100px;
+border-top: 2px solid #000;
+}
+
+.whisker_left span:nth-child(3) {
+left: 50px;
+-webkit-transform: rotate(-20deg);
+transform: rotate(-20deg);
+}
+
+.whisker_right span:nth-child(3) {
+right: 50px;
+-webkit-transform: rotate(20deg);
+transform: rotate(20deg);
+}
+
+.collar {
+position: absolute;
+top: 350px;
+left: 50px;
+width: 320px;
+height: 50px;
+background-color: #FF0000;
+border: 2px solid #000;
+border-radius: 30px;
+}
+
+.bell_01 {
+position: absolute;
+top: 15px;
+left: 130px;
+width: 60px;
+height: 60px;
+background-color: #FFFF00;
+border: 2px solid #000;
+border-radius: 50%;
+}
+.bell_02 {
+position: absolute;
+top: 17px;
+left: 1px;
+width: 53px;
+height: 18px;
+border-top: 2px solid #000;
+}
+
+.bell_03 {
+position: absolute;
+top: 22px;
+left: 0;
+width: 57px;
+height: 23px;
+border-top: 2px solid #000;
+}
+
+.bell_04 {
+position: absolute;
+top: 30px;
+left: 20px;
+width: 15px;
+height: 10px;
+background-color: #000;
+border-radius: 50%;
+}
+
+.bell_05 {
+position: absolute;
+top: 37px;
+left: 27px;
+width: 30px;
+height: 20px;
+border-left: 2px solid #000;
+}
+
+@keyframes wave{
+0%{
+transform: rotate(0deg);
+}
+33%{
+transform: rotate(15deg);
+}
+66%{
+transform: rotate(-15deg);
+}
+100%{
+transform: rotate(0deg);
+}
+}
+.bell_01:hover{
+animation: wave 300ms infinite linear;
+}
+`
+
+const player = {
+    id: undefined,
+    time: 100,
+    ui: {
+        demo: document.querySelector('#demo'),
+        demo2: document.querySelector('#demo2')
+    },
+    events: {
+        '#btnPause': 'pause',
+        '#btnPlay': 'play',
+        '#btnSlow': 'slow',
+        '#btnNormal': 'normal',
+        '#btnFast': 'fast'
+    },
+    n: 1,
+    init: () => {
+        player.ui.demo.innerText = string.substr(0, player.n)
+        player.ui.demo2.innerHTML = string.substr(0, player.n)
+        player.bindEvents()
+        player.play()
+    },
+    bindEvents: () => {
+        for (let key in player.events) {
+            if (player.events.hasOwnProperty(key)) {
+                const value = player.events[key] // pause / play / slow
+                document.querySelector(key).onclick = player[value]
+            }
+        }
+
+    },
+    run: () => {
+        player.n += 1
+        if (player.n > string.length) {
+            window.clearInterval(player.id)
+            return
+        }
+        player.ui.demo.innerText = string.substr(0, player.n)
+        player.ui.demo2.innerHTML = string.substr(0, player.n)
+        player.ui.demo.scrollTop = player.ui.demo.scrollHeight
+    },
+    play: () => {
+        window.clearInterval(player.id)
+        player.id = setInterval(player.run, player.time)
+    },
+    pause: () => {
+        window.clearInterval(player.id)
+    },
+    slow: () => {
+        player.pause()
+        player.time = 300
+        player.play()
+    },
+    normal: () => {
+        player.pause()
+        player.time = 100
+        player.play()
+    },
+    fast: () => {
+        player.pause()
+        player.time = 0
+        player.play()
+    }
+}
+
+player.init()
